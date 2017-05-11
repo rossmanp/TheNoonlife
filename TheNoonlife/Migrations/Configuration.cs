1b624fce5c02fa3c51135549687bafa4fe220952
Pete Rossman <pete.rossman@gmail.com>
2017-05-11 12:57:33 -0400

    Updated database changes in migration

--- TheNoonlife/Migrations/Configuration.cs
+++ TheNoonlife/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace TheNoonlife.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationsEnabled = false;
         }
 
         protected override void Seed(TheNoonlife.Models.ApplicationDbContext context)
